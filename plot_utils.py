8fd5997650a87ad768bbdbe383bdd39e7e9a0fca
theojeannes <theo.jeannes@etu.unice.fr>
2023-10-17 01:51:26 +0200

    Plot policies properly

--- plot_utils.py
+++ plot_utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
@@ -33,30 +32,30 @@ def plot_blackjack_values(V):
     get_figure(False, ax)
     plt.show()
 
-
 def plot_policy(policy):
-    def get_Z(x, y, usable_ace):
-        if (x, y, usable_ace) in policy:
-            return policy[x, y, usable_ace]
+    def get_Z(x,y, usable_ace):  # Swapped x and y
+        if (x, y, usable_ace) in policy:  # Swapped x and y
+            return policy[(x, y, usable_ace)]  # Swapped x and y
         else:
             return 1
 
     def get_figure(usable_ace, ax):
-        x_range = np.arange(11, 22)
-        y_range = np.arange(10, 0, -1)
-        X, Y = np.meshgrid(x_range, y_range)
-        Z = np.array([[get_Z(x, y, usable_ace) for x in x_range] for y in y_range])
-        surf = ax.imshow(Z, cmap=plt.get_cmap('Pastel2', 2), vmin=0, vmax=1, extent=[10.5, 21.5, 0.5, 10.5])
-        plt.xticks(x_range)
-        plt.yticks(y_range)
-        plt.gca().invert_yaxis()
-        ax.set_xlabel('Player\'s Current Sum')
-        ax.set_ylabel('Dealer\'s Showing Card')
+        x_range = np.arange(1,11)
+        y_range = np.arange(21,9,-1)
+        X,Y = np.meshgrid(x_range, y_range)  # Swapped X and Y
+        Z = np.array([get_Z(x,y,usable_ace) for x,y in zip(np.ravel(X), np.ravel(Y))]).reshape(X.shape)
+        surf = ax.imshow(Z, cmap=plt.get_cmap('Pastel2', 2), vmin=0, vmax=1, extent=[0.5, 10.5,9.5, 21.5])
+        plt.xticks(x_range)  # Swapped x_range with y_range
+        plt.yticks(y_range)  # Swapped x_range with y_range
+        # plt.gca().invert_yaxis()
+        ax.set_xlabel('Dealer\'s Showing Card')  # Swapped x and y labels
+        ax.set_ylabel('Player\'s Current Sum')  # Swapped x and y labels
         ax.grid(color='w', linestyle='-', linewidth=1)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.1)
-        cbar = plt.colorbar(surf, ticks=[0, 1], cax=cax)
+        cbar = plt.colorbar(surf, ticks=[0,1], cax=cax)
         cbar.ax.set_yticklabels(['0 (STICK)', '1 (HIT)'])
+        cbar.ax.invert_yaxis()
 
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(121)
